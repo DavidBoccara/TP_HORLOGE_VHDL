e83e87cc7c7555bab39fb6ceb757dd249280315b
Toshley2675 <davidboccara@hotmail.com>
2020-10-07 14:38:30 +0200

    FINALY_PROJECT

--- sources/ctrl_tb.vhd
+++ sources/ctrl_tb.vhd
@@ -11,7 +11,7 @@ architecture 	testbench of ctrl_tb is
     constant period      : time := 0.02 sec;
     signal clk     : std_logic := '1';
     signal rst    : std_logic := '0';
-    signal top_1s : std_logic :;
+    signal top_1s : std_logic;
 
     signal seconde_A            :    std_logic_vector(3 downto 0) := "0000";
     signal seconde_B           :   std_logic_vector(3 downto 0) := "0000";
@@ -41,24 +41,32 @@ begin
                 push2  => push2,
                 mode   => mode
                 );
-        impulsion : entity work.impulsion(rtl)
-            generic map(CLK_DIVIDER => CLK_DIVIDER)
-            port map(
-                clk => clk,
-                rst => rst,
-                top_1s => top_1s);
+
+
       
 
 		clk <= not clk after period / 2;
-    process is
+    entree1 : process
     begin
 
  
         wait until rising_edge(clk);
         wait until rising_edge(clk);
  
-  
+      
+        rst <= '0';
+		
+		
+ 
         wait;
-    end process;		
+    end process entree1;
+
+	entree2 : process	
+	begin
+		top_1s <= '0';
+		wait for 990 ms;
+		top_1s <= '1';
+		wait for 10 ms;			--valeur reel : 10 ns
+	end process entree2;	
 
 end architecture ; -- 	testbench
